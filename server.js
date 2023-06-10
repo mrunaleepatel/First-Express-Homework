2252133d7f2af9c78adf13dc2ba60f6192fa9fa4
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-06-10 15:14:38 -0500

    Greeting express application created

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
+require("dotenv").config()
 const express = require("express") 
+const app = express() 
+const PORT = process.env.PORT
 
-const app = express()
-
-app.get("/" , (req, res) => {
-    res.send("Hello Stranger")
-})
-
-app.get('/greeting/:name', (req, res) => {
-    const name = req.params.name;
-    const greeting = `Hello there, ${name}!`;
-    res.send(greeting);
-  });
-
-app.get('/tip/:total/:tipPercentage', (req, res) => {
-    const total = parseFloat(req.params.total);
-    const tipPercentage = parseFloat(req.params.tipPercentage);
-    const tipAmount = (total * tipPercentage) / 100;
-    res.send(tipAmount.toString());
+app.get('/greeting/:name?', (req, res) => {
+    const name = req.params.name || 'stranger';
+    const message = name === 'stranger' ? 'Hello, stranger' : `Wow! Hello there, ${name}`;
+  
+    res.send(message);
   });
 
 
-app.listen(3000,() => {
-    console.log("Listening on port 3000")
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`LISTENING ON PORT ${PORT}`)
 })
