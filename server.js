ab11352d12f144f96abef2b7652948fdb891f373
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-06-10 15:22:45 -0500

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.get('/greeting/:name?', (req, res) => {
     res.send(message);
   });
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const total = Number(req.params.total);
+    const tipPercentage = Number(req.params.tipPercentage);
+  
+    const tipAmount = (total * tipPercentage) / 100;
+  
+    res.send(tipAmount.toString());
+  });
 
 
 
